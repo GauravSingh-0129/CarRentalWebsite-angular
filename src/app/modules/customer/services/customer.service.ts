a4b400175fee57155b27151d1eec3ae29512488f
Gaurav Singh <singh.gaurav0129@gmail.com>
2024-03-30 19:27:12 +0530

    Added customer dashboard...

--- src/app/modules/customer/services/customer.service.ts
+++ src/app/modules/customer/services/customer.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { StorageService } from 'src/app/auth/services/storage/storage.service';
+
+const BASIC_URL = ["http://localhost:8080"];
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  getAllCars(): Observable<any>{
+    return this.http.get(BASIC_URL + "/api/customer/cars", {
+      headers : this.createAuthorization()
+    });
+  }
+
+  createAuthorization(): HttpHeaders{
+    let authHeaders : HttpHeaders = new HttpHeaders();
+    console.log(StorageService.getToken())
+    return authHeaders.set('Authorization', 'Bearer ' + StorageService.getToken()
+    );
+  }
 }
